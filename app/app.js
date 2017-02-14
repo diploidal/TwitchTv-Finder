fcf42a5c03d97a176b9cf405e3c134a1e14c3994
Diploidal <diploidal@gmail.com>
2017-02-14 12:13:03 +0100

    Styled landing page and created simple GET request from Twitch on btn click

--- app/app.js
+++ app/app.js
@@ -1,13 +1,15 @@
-$(document).ready(function() {
-    $('#searchUser').on('keyup', function(e) {
+document.addEventListener('click', function(){
+    document.getElementById('searchBtn');
+    
+    $(document).ready(function() {
+    $('#searchBtn').on('click', function(e) {
         let userName = e.target.value;
 
         // Twitch request
-
         $.ajax({
             type:'GET',
-            url:'https://api.twitch.tv/kraken/channels/lirik',
-                header:{
+            url:'https://api.twitch.tv/kraken/users/' + userName,
+                headers:{
                     'Client-ID':'5tdzz2mcd55pe9bww0uxrduwr44vc7',
             },
             success: function(data) {
@@ -17,3 +19,10 @@ $(document).ready(function() {
         });
     });
 });
+
+
+
+
+});
+
+
