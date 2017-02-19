58e6098e5e07326b65c374239e787225f9f99f9d
Maksymilian Pryliński <diploidal@gmail.com>
2017-02-19 19:22:15 +0100

    Added some paragraphs with additional info

--- app/app.js
+++ app/app.js
@@ -18,6 +18,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
                     'Client-ID': client_ID,
                 },
                 success: function(channel_info) {
+                  console.log(channel_URL);
                     console.log(channel_info);
                     $("#profile").html(`
                      <div class="container">
@@ -26,24 +27,25 @@ document.addEventListener('DOMContentLoaded', function(event) {
                           <img class="activator" src="${channel_info.logo}">
                         </div>
                         <div class="card-content">
-                          <span class="card-title activator grey-text text-darken-4">${channel_info.display_name}<i class="material-icons right">more_vert</i></span>
+                          <span class="card-title activator grey-text text-darken-4"><b>${channel_info.display_name}</b><i class="material-icons right">more_vert</i></span>
 
                           <p>${channel_info.status}
                         </div>
 
                         <div class="card-action">
-                          <a href="${channel_info.url}" target="_blank">Watch LIVE</a>
-                          <a href="https://www.twitch.tv" target="_blank">Check Twitch.tv</a>
+                          <b><a href="${channel_info.url}" target="_blank">Watch LIVE</a></b>
+                          <b><a href="https://www.twitch.tv" target="_blank">Check Twitch.tv</a></b>
                         </div>
 
                         <div class="card-reveal">
                           <span class="card-title grey-text text-darken-4">${channel_info.display_name}<i class="material-icons right">close</i></span>
                           <p>${channel_info.status}</p>
                           <div class="divider"></div>
-                          <p><b>Game:</b> ${channel_info.game}</p>
+                          <p><b>Playing:</b> ${channel_info.game}</p>
                           <p><b>Followers:</b> ${channel_info.followers}</p>
                           <p><b>Total views:</b> ${channel_info.views}</p>
                           <p><b>Broadcaster language:</b> ${channel_info.broadcaster_language}</p>
+                          <p><b>For mature:</b> ${channel_info.partner}</p>
                         </div>
                       </div>
                      </div>
@@ -52,12 +54,4 @@ document.addEventListener('DOMContentLoaded', function(event) {
             });
         });
     });
-
-
-
-
-
-
-
-
 });
