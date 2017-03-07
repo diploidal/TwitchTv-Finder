a4a2f195baf122f3eeb4e7eb56710ee279fcb72c
Diploidal <diploidal@gmail.com>
2017-03-07 12:47:46 +0100

    Separated channel info with additional info tabs

--- app/app.js
+++ app/app.js
@@ -37,65 +37,34 @@ document.addEventListener('DOMContentLoaded', function (event) {
                               <div class="col s12 m6"><img src="${channel_info.logo}"></div>
                               <div class="col s12 m6"><p class="flow-text bold">${channel_info.display_name}</p></div>
                               <div class="col s12 m6"><p><b>Status:</b> ${channel_info.status}</p></div>
-                              <div class="col s12 m6"><p><b>Playing:</b> ${channel_info.game}</p>
-                                <div class="divider"></div>
-                              </div>
-                              <div class="col s12 m6"><p id="updated"><b>Status updated: </b></p></div>
-                              <div class="col s12 m6"><p id="partner"><b>Partnered: </b></p></div>
-                              <div class="col s12 m6"><p id="created"><b>Channel created:</b> </p></div>
-                              <div class="col s12 m6"><p id="follower-ammount"><b>Followers:</b> </p></div>
-                              <div class="col s12 m6"><p id="total-views"><b>Total views: </b> </p></div>
-                              <div class="col s12 m6"><p><b>Broadcaster language: </b>${channel_info.broadcaster_language}</p></div>
-
-
-
-
+                              <div class="col s12 m6"><p><b>Playing:</b> ${channel_info.game}</p></div>
                             </div>
                         </div>
 
 
-                            <div id="stream" class="col s12 m2 l2">Test 2</div>
-                            <div id="addinfo" class="col s12 m2 l2">Test 3</div>
+
+
+                        <div class="row">
+                          <div id="addinfo" class="col s12 center">
+                            <div class="col s12 m6"><img src="${channel_info.logo}"></div>
+                            <div class="col s12 m6"><p class="flow-text bold">${channel_info.display_name}</p></div>
+                            <div class="col s12 m6"><p><b>Status:</b> ${channel_info.status}</p></div>
+                            <div class="col s12 m6"><p><b>Playing:</b> ${channel_info.game}</p>
+                              <div class="divider"></div>
+                            </div>
+                            <div class="col s12 m6"><p id="updated"><b>Status updated: </b></p></div>
+                            <div class="col s12 m6"><p id="partner"><b>Partnered: </b></p></div>
+                            <div class="col s12 m6"><p id="created"><b>Channel created:</b> </p></div>
+                            <div class="col s12 m6"><p id="follower-ammount"><b>Followers:</b> </p></div>
+                            <div class="col s12 m6"><p id="total-views"><b>Total views: </b> </p></div>
+                            <div class="col s12 m6"><p><b>Broadcaster language: </b>${channel_info.broadcaster_language}</p></div>
+                          </div>
+                        </div>
+
+            
                           </div>
                        </div>
 
-
-
-
-
-
-                      <div class="card sticky-action hoverable">
-                        <div class="card-image waves-effect waves-block waves-light">
-                          <img class="activator" src="${channel_info.logo}">
-                        </div>
-                        <div class="card-content">
-                          <span class="card-title activator grey-text text-darken-4"><b>${channel_info.display_name}</b>
-                          <i class="material-icons right">more_vert</i></span>
-
-                          <p>${channel_info.status}</p>
-                        </div>
-
-                        <div class="card-action">
-                          <b><a href="${channel_info.url}" target="_blank">Watch ${channel_info.display_name} LIVE</a></b>
-                          <b><a href="https://www.twitch.tv" target="_blank" class="right">Check Twitch.tv</a></b>
-                        </div>
-
-                        <div class="card-reveal">
-                          <span class="card-title grey-text text-darken-4">${channel_info.display_name}<i class="material-icons right">close</i></span>
-                          <p>${channel_info.status}</p>
-                          <p id="updated"><b>Updated status at: </b></p> 
-                          <div class="divider"></div>
-                          <p><b>Playing:</b> ${channel_info.game}</p>
-                          <p><b>Followers:</b> ${channel_info.followers}</p>
-                          <p><b>Total views:</b> ${channel_info.views}</p>
-                          <p><b>Broadcaster language:</b> ${channel_info.broadcaster_language}</p>
-                          <p id="partner"><b>Partnered:</b></p>
-                          <p id="created"><b>Channel created:</b></p>
-                        </div>
-                      </div>
-
-
-                     </div> 
                     `);
 
           // STATUS CHANGE TIME
